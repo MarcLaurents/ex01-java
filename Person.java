ccb850235f3422be7c359372dab08a7af12e1593
Marcelo Melo <90254883+MarcLaurents@users.noreply.github.com>
2024-03-18 08:41:48 -0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.lastName + " " + this.name;
+    }
+}
