a0c06a06fb5b57c9a44a6677c2e099a0fd43500b
Marcelo Melo <90254883+MarcLaurents@users.noreply.github.com>
2024-03-18 08:42:41 -0300

    Create Professor.java

---
+++ Professor.java
@@ -0,0 +1,9 @@
+public class Professor extends Employee {
+    public double getFirstPaySalary() {
+        return this.getSalary();
+    }
+
+    public double getSecondPaySalary() {
+        return 0.0;
+    }
+}
